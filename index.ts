2275be37a30dc1cc712ca93d10cd3d8dba5aaa44
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2023-10-09 01:44:39 +0200

    Load built-in middlewares

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import express, {Express, Request, Response} from "express";
+import express, {Express, Request, Response, urlencoded} from "express";
 import path from "path"
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -14,6 +14,12 @@ dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT;
+
+// Loading built-in middlewares
+app.use(logger('dev'));
+app.use(express.json()); // to help populate req.body with form data
+app.use(express,urlencoded({ extended: false })); // populate req.body with query parameters
+app.use(cookieParser())
 
 // Loading the application routes
 app.use("/topics", topics_router);
